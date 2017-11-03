3681265aea8e2a9ecde1501c53a4efaa9e300f6d
dpisdaniel <daniel.pakula102@gmail.com>
2017-11-03 18:14:33 +0200

    add VGA constant properties

--- src/consts.asm
+++ src/consts.asm
@@ -1,7 +1,9 @@
+; loliOS - a 32 bit OS written in Assembly
+; Copyright (C) 2017 dpisdaniel -- see LICENSE
 
 ; Constant system strings
-sys_version:            db 'loliOS ver 0.1', 0
-ready_message:          db 'loliOS is ready', 13, 0
+SYS_VERSION:            db 'loliOS ver 0.1', 0
+READY_MESSAGE:          db 'loliOS is ready', 13, 0
 
 ; Mem addresses
 BIOS_MMAP               equ 0x00020000 ; 24 bytes per entry, BIO_MMAP_LENGTH x 24 bytes
@@ -9,7 +11,7 @@ BIOS_MMAP_LENGTH        equ 0x00021000 ; 2 bytes
 KERNEL_START            equ 0x00008000 ; TODO: Add padding to wanted length
 
 ; Color pallete
-vga_colors:
+VGA_COLORS:
 db 0  ; black
 db 1  ; blue
 db 2  ; green
@@ -28,8 +30,10 @@ db 14 ; light brown
 db 15 ; white
 
 ; Constant addresses
-vga_buffer:             equ 0x000B8000
-; Byte global variables DB
-vga_width:  equ 80
-vga_height: equ 25
+VGA_BUFFER:             equ 0x000B8000
+; Constant values
+VGA_WIDTH  equ 80
+VGA_HEIGHT equ 25
+VGA_DEF_BG equ 00 ; black - 4 bits
+VGA_DEF_FG equ 01 ; blue  - 4 bits
 
